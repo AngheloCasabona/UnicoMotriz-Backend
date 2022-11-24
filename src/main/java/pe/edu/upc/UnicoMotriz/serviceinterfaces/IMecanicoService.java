4a7fb9f19325090b0c13e86a6bcf6b2bb37960f9
KevinYSV <u201924660@upc.edu.pe>
2022-11-24 11:22:53 -0500

    CRUD2-MECANICO Y TALLER

--- src/main/java/pe/edu/upc/UnicoMotriz/serviceinterfaces/IMecanicoService.java
+++ src/main/java/pe/edu/upc/UnicoMotriz/serviceinterfaces/IMecanicoService.java
@@ -5,9 +5,10 @@ import pe.edu.upc.UnicoMotriz.entities.Mecanico;
 import java.util.List;
 import java.util.Optional;
 public interface IMecanicoService{
-    public void insert(Mecanico mecanico);
+    public boolean insert(Mecanico mecanico);
     List<Mecanico>list();
-    public void delete(int idMecanico);
-    public Optional<Mecanico> listId(int idMecanico);
-    List<Mecanico> findName(String nameMecanico);
+    public void delete(int cmecanico);
+    public Optional<Mecanico> listId(int cmecanico);
+    List<Mecanico> buscarMecanico(String nmecanico);
+    List<Mecanico> buscarTaller(String ntaller);
 }
